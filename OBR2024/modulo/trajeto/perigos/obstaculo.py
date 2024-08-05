534d13a8b7ee0b44c63802803bf827f7f7f6c841
kauegh <kaueguilhermebf2019@gmail.com>
2024-08-05 19:56:06 -0300

    percurso do obstaculo para testar

--- OBR2024/modulo/trajeto/perigos/obstaculo.py
+++ OBR2024/modulo/trajeto/perigos/obstaculo.py
@@ -5,3 +5,40 @@ from ...robo import robo
 # Desvia do obstáculo
 def obstaculo():
     print("Desviando...")
+    robo.bz.settings(100)
+
+    while True:
+        if robo.ultrassonicoFrente.distance() > 45:
+           robo.bz.drive(100)
+        else:
+            robo.bz.straight(-40)
+            robo.bz.turn(90)
+            robo.bz.straight(-50)
+
+            while True:
+                if robo.ultrassonicoLado.distance() <= 70:
+                    robo.bz.drive(70)
+                else:
+                    robo.bz.straight(60)
+                    robo.bz.turn(-90)
+
+                    while True:
+                        if robo.ultrassonicoLado.distance() <= 150:
+                             robo.bz.drive(70)
+                        else:
+                            robo.bz.straight(60)
+                            robo.bz.turn(-90)
+
+                            while robo.sensorCorEsquerda.color() != robo.Color.BLACK:
+                                robo.bz.drive(80)
+                                break
+                            break
+                        break
+
+
+                        
+                             
+                        
+
+
+
