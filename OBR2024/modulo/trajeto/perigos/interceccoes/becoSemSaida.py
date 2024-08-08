54f87ea9bb10020d1f4d3f1b3a1b7b9c232beacf
kauegh <kaueguilhermebf2019@gmail.com>
2024-08-08 16:52:01 -0300

    beco sem saida teste

--- OBR2024/modulo/trajeto/perigos/interceccoes/becoSemSaida.py
+++ OBR2024/modulo/trajeto/perigos/interceccoes/becoSemSaida.py
@@ -10,3 +10,32 @@ def becoSemSaida():
     '''
     
     print("meia volta")
+
+    while True:
+        print('os dois sensores detectam o verde')
+        if robo.sensorCorEsquerda.color() and robo.sensorCorDireita.color() == robo.Color.GREEN:
+            print('os dois sensores detectam o preto, e o robo anda 8cm para frente')
+            while robo.sensorCorEsquerda.color() and robo.sensorCorDireita.color() != robo.Color.BLACK:
+                robo.bz.drive(80)
+            
+            else:
+                robo.bz.straight(50)
+                print(" robo gira até o sensor da esquerda ver o preto")
+                while robo.sensorCorEsquerda.color() != robo.Color.BLACK:
+                    robo.bz.stop()
+                    robo.motorEsquerdo.run(-50)
+                
+                else:
+                    robo.wait(2000)
+                    robo.motorEsquerdo.angle(-150)
+                    print(" robo gira até o sensor da esquerda ver o preto(linha)")
+                    while robo.sensorCorEsquerda.color() != robo.Color.BLACK:
+                        robo.motorEsquerdo.run(-50)
+
+                    else:
+                        robo.wait(2000)
+                        robo.motorEsquerdo.angle(-150)
+
+
+
+        break
