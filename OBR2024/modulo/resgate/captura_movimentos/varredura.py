089b2d8bacc0430b8cca9fb7603fad6e025354dc
kauegh <kaueguilhermebf2019@gmail.com>
2024-08-15 10:10:09 -0300

    pasta de movimentos de captura

---
+++ OBR2024/modulo/resgate/captura_movimentos/varredura.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env pybricks-micropython
+
+#Importações
+#______________________________________________________________________________________________________________________________________
+from ...robo import robo
+
+
+def varredura():
+    print("iniciando varredura")
